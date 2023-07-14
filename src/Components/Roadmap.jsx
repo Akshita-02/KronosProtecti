292c7df67a8fe6cfb696d92d532824a8eba3ac02
Tribhuwan <tribhuwanja@gmail.com>
2023-07-14 14:29:15 +0530

    Roadmap Function is compleated successfully

--- src/Components/Roadmap.jsx
+++ src/Components/Roadmap.jsx
@@ -31,8 +31,11 @@ const Roadmap = () => {
         </h1>
         <p>
           Providing security services involves a range of activities aimed at
-          protecting people, prperty, </p> <p>and assests from potential threatss. Here
-          are some general steps to consider when providing securrity services:
+          protecting people, prperty,{" "}
+        </p>{" "}
+        <p>
+          and assests from potential threatss. Here are some general steps to
+          consider when providing securrity services:
         </p>
       </div>
 
@@ -43,6 +46,83 @@ const Roadmap = () => {
           height="700"
           style={{ color: "blueviolet" }}
         ></canvas>
+      </div>
+
+      <div className="roadmap-content">
+        <div className="ct1 ctm">
+          <img
+            src="../../image/Screenshot 2023-07-14 133917.png"
+            alt=""
+          />
+          <p> Asseess client need</p>
+        </div>
+        <div className="ct1 ctm">
+          <img
+            src="../../image/2.png"
+            alt=""
+          />
+          <p> develop a security plan</p>
+        </div>
+        <div className="ct2 ctm">
+          <img
+            src="../../image/3.png"
+            alt=""
+          />
+          <p> Hire and train security personal</p>
+        </div>
+        <div className="ct3 ctm">
+          <img
+            src="../../image/4.png"
+            alt=""
+          />
+          <p> Emergency response planing</p>
+        </div>
+      </div>
+      <div className="ct-sec-2">
+        <div className="ct4 ctm">
+          <img
+            src="../../image/6.png"
+            alt=""
+          />
+          <p> Impliment security</p>
+        </div>
+        <div className="ct4 ctm">
+          <img
+            src="../../image/7.png"
+            alt=""
+          />
+          <p> Monitoring and surveillance</p>
+        </div>
+        <div className="ct4 ctm">
+          <img
+            src="../../image/8.png"
+            alt=""
+          />
+          <p> Maintain through records of all security incidents</p>
+        </div>
+      </div>
+      <div className="ct-sec-3">
+        <div className="ct6 ctm">
+          <img
+            src="../../image/9.png"
+            alt=""
+          />
+          <p>Contact regular reviews of the security plan</p>
+        </div>
+        <div className="ct6 ctm">
+          <img
+            src="../../image/10.png"
+            alt=""
+          />
+          <p> Continous training and development</p>
+        </div>
+        <div className="ct6 ctm">
+          <img
+            src="../../image/5.png"
+            alt=""
+          />
+          <p> Monitoring and surveillance</p>
+        </div>
       </div>
     </>
   );
