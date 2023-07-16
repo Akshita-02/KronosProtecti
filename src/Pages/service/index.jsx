7eb1af534fb2c537d3557d5593ede67003a0724e
Tribhuwan <tribhuwanja@gmail.com>
2023-07-16 14:24:51 +0530

    service page compleated

--- src/Pages/service/index.jsx
+++ src/Pages/service/index.jsx
@@ -5,111 +5,221 @@ const Service = () => {
   return (
     <>
       <Cta />
-          <div
-            className="service-section"
-            style={{
-              background: "rgb(9, 8, 27)",
-              // eslint-disable-next-line
-              background:
-                "linear-gradient(45deg, rgba(9, 8, 27, 1) 59%, rgba(9, 9, 121, 1) 75%, rgba(0, 212, 255, 0.5) 100%)",
-            }}
-          >
-            <div className="ssh-font">
-              <h2>
-                Take a Look At <span>Services We Provided</span>
-              </h2>
-              <p>We are to protect with courage & to serve with compassion.</p>
-              <p>We specialize in security and armed bodyguard service.</p>
+      <div
+        className="service-section"
+        style={{
+          background: "rgb(9, 8, 27)",
+          // eslint-disable-next-line
+          background:
+            "linear-gradient(45deg, rgba(9, 8, 27, 1) 59%, rgba(9, 9, 121, 1) 75%, rgba(0, 212, 255, 0.5) 100%)",
+        }}
+      >
+        <div className="ssh-font">
+          <h2>
+            Take a Look At <span>Services We Provided</span>
+          </h2>
+          <p>We are to protect with courage & to serve with compassion.</p>
+          <p>We specialize in security and armed bodyguard service.</p>
+        </div>
+        <div className="service-card">
+          <div className="service1">
+            <div className="logo">
+              <img src="../../../image/s-logo1.png" alt="" />
             </div>
-            <div className="service-card">
-              <div className="service1">
-                <div className="logo">
-                  <img src="../../../image/s-logo1.png" alt="" />
-                </div>
-                <h3>Private Investigation Service</h3>
-                <p>
-                  We can help you find legal and efficient solutions to your
-                  problems using expert private investigator{" "}
-                </p>
-              </div>
-              <div className="service2">
-                <div className="logo">
-                  <img src="../../../image/s-logo2.png" alt="" />
-                </div>
-                <h3>Private Investigation Service</h3>
-                <p>
-                  We can help you find legal and efficient solutions to your
-                  problems using expert private investigator{" "}
-                </p>
-              </div>
-              <div className="service3">
-                <div className="logo">
-                  <img src="../../../image/s-logo3.png" alt="" />
-                </div>
-
-                <h3>Private Investigation Service</h3>
-                <p>
-                  We can help you find legal and efficient solutions to your
-                  problems using expert private investigator{" "}
-                </p>
-              </div>
-              <div className="service4">
-                <div className="logo">
-                  <img src="../../../image/s-logo4.png" alt="" />{" "}
-                </div>
-                <h3>Private Investigation Service</h3>
-                <p>
-                  We can help you find legal and efficient solutions to your
-                  problems using expert private investigator{" "}
-                </p>
-              </div>
-              <div className="service5">
-                <div className="logo">
-                  <img src="../../../image/s-logo5.png" alt="" />
-                </div>{" "}
-                <h3>Private Investigation Service</h3>
-                <p>
-                  We can help you find legal and efficient solutions to your
-                  problems using expert private investigator{" "}
-                </p>
-              </div>
-              <div className="service6">
-                <div className="logo">
-                  <img src="../../../image/s-logo6.png" alt="" />
-                </div>
-                <h3>Private Investigation Service</h3>
-                <p>
-                  We can help you find legal and efficient solutions to your
-                  problems using expert private investigator{" "}
-                </p>
-              </div>
-              <div className="service7">
-                <div className="logo">
-                  <img src="../../../image/s-logo7.png" alt="" />
-                </div>
-                <h3>Private Investigation Service</h3>
-                <p>
-                  We can help you find legal and efficient solutions to your
-                  problems using expert private investigator{" "}
-                </p>
-              </div>
-              <div className="service8">
-                <div className="logo">
-                  <img src="../../../image/s-logo8.png" alt="" />
-                </div>
-                <h3>Private Investigation Service</h3>
-                <p>
-                  We can help you find legal and efficient solutions to your
-                  problems using expert private investigator{" "}
-                </p>
-              </div>
-            </div>
+            <h3>Private Investigation Service</h3>
+            <p>
+              We can help you find legal and efficient solutions to your
+              problems using expert private investigator{" "}
+            </p>
           </div>
-          <div className="events-section">
-            <img src="../../../image/events.jpeg" alt="" />
-          </div>{" "}
+          <div className="service2">
+            <div className="logo">
+              <img src="../../../image/s-logo2.png" alt="" />
+            </div>
+            <h3>Private Investigation Service</h3>
+            <p>
+              We can help you find legal and efficient solutions to your
+              problems using expert private investigator{" "}
+            </p>
+          </div>
+          <div className="service3">
+            <div className="logo">
+              <img src="../../../image/s-logo3.png" alt="" />
+            </div>
+
+            <h3>Private Investigation Service</h3>
+            <p>
+              We can help you find legal and efficient solutions to your
+              problems using expert private investigator{" "}
+            </p>
+          </div>
+          <div className="service4">
+            <div className="logo">
+              <img src="../../../image/s-logo4.png" alt="" />{" "}
+            </div>
+            <h3>Private Investigation Service</h3>
+            <p>
+              We can help you find legal and efficient solutions to your
+              problems using expert private investigator{" "}
+            </p>
+          </div>
+          <div className="service5">
+            <div className="logo">
+              <img src="../../../image/s-logo5.png" alt="" />
+            </div>{" "}
+            <h3>Private Investigation Service</h3>
+            <p>
+              We can help you find legal and efficient solutions to your
+              problems using expert private investigator{" "}
+            </p>
+          </div>
+          <div className="service6">
+            <div className="logo">
+              <img src="../../../image/s-logo6.png" alt="" />
+            </div>
+            <h3>Private Investigation Service</h3>
+            <p>
+              We can help you find legal and efficient solutions to your
+              problems using expert private investigator{" "}
+            </p>
+          </div>
+          <div className="service7">
+            <div className="logo">
+              <img src="../../../image/s-logo7.png" alt="" />
+            </div>
+            <h3>Private Investigation Service</h3>
+            <p>
+              We can help you find legal and efficient solutions to your
+              problems using expert private investigator{" "}
+            </p>
+          </div>
+          <div className="service8">
+            <div className="logo">
+              <img src="../../../image/s-logo8.png" alt="" />
+            </div>
+            <h3>Private Investigation Service</h3>
+            <p>
+              We can help you find legal and efficient solutions to your
+              problems using expert private investigator{" "}
+            </p>
+          </div>
+        </div>
+      </div>
       <div className="events-section">
         <img src="../../../image/events.jpeg" alt="" />
+      </div>{" "}
+      <div className="event-details">
+        <h2>
+          <span>
+            {" "}
+            <strong>ONTARIO </strong> Events secured by{" "}
+          </span>
+        </h2>
+        <h1 className="events-h">KRONOS SECURITY</h1>
+      </div>
+      <div className="event-card">
+        <div className="cd-1">
+          <li>THe Moose</li>
+          <li>Beer Garden</li>
+          <li>Parkway Social</li>
+          <li>Sky Bar</li>
+          <li>Dragon Fly</li>
+          <li>Mansion</li>
+          <li>Lounge 33</li>
+          <li>Bad & Boujee</li>
+        </div>
+        <div className="cd-2">
+          <li>Comic con Toronto</li>
+          <li>Comic con Hamilton</li>
+          <li>420 expo</li>
+          <li>Frightmare</li>
+          <li>Int Gin festival</li>
+          <li>Oktoberfest</li>
+          <li>Riverfest</li>
+          <li>Ancaster fair</li>
+          <li> Comic con Niagara</li>
+        </div>
+        <div className="cd-3">
+          <li> Canal day's </li>
+          <li> Festival NHL </li>
+          <li> Canada day festival</li>
+          <li> BTC MMA</li>
+          <li> United MMA </li>
+          <li> 3 Kings Boxing</li>
+          <li>Greycup 2021</li>
+          <li>Winter clasic 2021</li>
+          <li> Canada vs USA</li>
+          <li> World cup</li>
+        </div>
+      </div>
+      <div className="risk-sec">
+        <h3>
+          Prepared For Anything: <span> Risk Assessment</span>
+        </h3>
+        <h3>
+          <span> And Emergency Prevention </span>
+        </h3>
+        <div className="risk-detail">
+          <div className="detail-l">
+            <p>
+              {" "}
+              Risk assesment and emergency preventation are indispensable
+              practice for ensuring safty and minimizing the impact of
+              emergencies. by Identifying and analyzing potential risks and
+              hazards, individuals and organizations can implement preventive
+              measures that safeguard lives, assets, and the environment.
+            </p>
+            <p>
+              We focuses on taking proactive measures to reduce the likelihood
+              of emergencies and minimize their potential impact. it involves
+              implementing preventive strategies and measures based on the
+              outcomes of risk assessments.
+            </p>
+          </div>
+          <div className="detail-r">
+            <img src="../../../image/event-img.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="k-protection">
+        <h1 className="hk-1"> "Your world is more secure with us" </h1>
+        <div className="hk">
+          <p>Close security by Kronos Protection</p>
+        </div>
+        <div className="img">
+          <img src="../../../image/close-protect.png" alt="" />
+        </div>
+        {/* //proteection card starts here  */}
+        <div className="protection-card">
+          <div className="p-c-1">
+            <h1>Artist</h1>
+            <li>Pink</li>
+            <li>Dark</li>
+            <li>Taylor Swift</li>
+            <li>Shaw Mendez</li>
+            <li>Lil John</li>
+            <li>Swae Lee</li>
+          </div>
+          <div className="p-c-2">
+            <h1>bands</h1>
+            <li>Mostley Crue</li>
+            <li>Slaughter</li>
+            <li>Fire House</li>
+            <li>FleetWood Mac</li>
+            <li>Kiss</li>
+          </div>
+          <div className="p-c-3">
+            <h1>movie/tv</h1>
+            <li>Jersey Shore</li>
+            <li>Johnny Depp</li>
+            <li>Bruce Campbell</li>
+            <li>Denzel Washington</li>
+            <li>Margo Robbie</li>
+            <li>Jeff Goldboom</li>
+            <li>John Travolta</li>
+            <li>Lou Frigno</li>
+          </div>
+        </div>
       </div>
     </>
   );
