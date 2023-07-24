aa1c3237c029cd09d86f267a2372f41022ee13b2
Akshita-02 <75935750+Akshita-02@users.noreply.github.com>
2023-07-24 12:12:56 +0530

    Update Hero.js

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -43,17 +43,17 @@ const Hero = () => {
           </h1>
           <hr />
           <p>
-            "Security is essential in our ever changing world as it promotes a
+            "Security is essential in our ever-changing world as it promotes a
             safe and secure environment for those who enjoy festivals, events,
-            concerts and night life"
+            concerts and nightlife"
           </p>
           <p>
             Welcome to <strong>Kronos Security</strong> . We are a professional security
-            company built on trust, integrity and service excellence. Kronos
+            the company built on trust, integrity, and service excellence. Kronos
             security is an elite security provider specializing in risk
-            assessments, crisis management, safety and security concepts for
+            assessments, crisis management, safety, and security concepts for
             large or small events and facilities within the corporate,
-            instititional, retail and private sector.
+            institutional, retail, and private sector.
           </p>
           <button className="btn" onClick={handleContactClick}>
             Contact Us
